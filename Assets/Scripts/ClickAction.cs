37d137fa4ecd84aaf071414e83c2e10eb9181cf9
k4sr4 <k4sr4_r@yahoo.com>
2015-06-21 21:17:25 -0500

    Prototype 1
    I made a basic board where you can put your units on and move them
    around. No battles between units. Movement of units in not correct.

---
+++ Assets/Scripts/ClickAction.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickAction : MonoBehaviour {
+
+	GameObject selected;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			Ray toMouse = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit rhInfo;
+			bool didHit = Physics.Raycast (toMouse, out rhInfo);
+			if (StartScript.start){
+				if (didHit){
+					if (!isSelected() && rhInfo.collider.tag == "Unit"){
+						selected = rhInfo.collider.gameObject;
+					}
+					else if (isSelected() && selected.tag == "Unit"){
+						Swap (selected, rhInfo.collider.gameObject);
+						selected = null;
+					}
+					else if (rhInfo.collider.tag == "Grid"){
+						if (isSelected()){
+							changePosition (rhInfo.collider.gameObject.transform.position);
+						}
+					}
+					Debug.Log("Hitted " + rhInfo.collider.name);
+				}
+				else
+					Debug.Log("NO hit");
+			}
+			else if (!StartScript.start){
+				if (didHit){
+					if (rhInfo.collider.tag == "Grid"){
+						Instantiate(FindObjectOfType<UnitSelection>().GetToInstantiate(), rhInfo.collider.transform.position, Quaternion.identity);
+					}
+				}
+				else
+					Debug.Log("NO hit");
+			}
+
+		}
+	}
+
+	void Swap (GameObject first, GameObject second){
+		Vector3 temp;
+		temp = first.transform.position;
+		first.transform.position = second.transform.position;
+		second.transform.position = temp;
+	}
+
+	bool isSelected(){
+		if (selected == null)
+			return false;
+		else
+			return true;
+	}
+
+	void changePosition (Vector3 pos){
+		selected.transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/TextScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextScript : MonoBehaviour {
+
+	void Start()
+	{
+		GetComponent<TextMesh>().GetComponent<MeshRenderer>().sortingLayerName = "Forground";
+		GetComponent<TextMesh>().GetComponent<MeshRenderer>().sortingOrder = 99;
+	}
+}
