9ac5aa2ceef99b1933db463280349485e35cd6db
Ardeol <new0programmer@gmail.com>
2015-06-26 16:31:44 -0500

    Kasra's stuff

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	public static bool start = false;
+
+	public void StartGame(){
+		start = true;
+	}
+}
---
+++ Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UnitSelection : MonoBehaviour {
+	
+	public int power, defence;
+	public GameObject toInstantiate;
+
+	public void Select(){
+		SetToInstantiate(power, defence);
+	}
+
+	void SetToInstantiate (int pow, int def){
+		toInstantiate.transform.Find ("Power").GetComponent<TextMesh>().text = pow.ToString();
+		toInstantiate.transform.Find ("Defence").GetComponent<TextMesh>().text = def.ToString();
+	}
+
+	public GameObject GetToInstantiate(){
+		return toInstantiate;
+	}
+}
